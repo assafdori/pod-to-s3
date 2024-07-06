ee6c03d6973dc7229dece5785609d8b6447045ab
Assaf Dori <assafxdori@gmail.com>
2024-07-06 16:37:00 +0300

    poof

---
+++ README.md
@@ -0,0 +1,19 @@
+### Project Overview: AWS EKS upload a file to an S3 Bucket from a Kubernetes pod ⛴️
+
+#### Objective: Create a Kubernetes deployment for a Python application that uploads a file to an S3 bucket.
+#### Technologies: Python, boto3, Docker, Kubernetes, EKS, IRSA (IAM Roles for Service Accounts), eksctl/Terraform.
+
+##### Highlight: This is highly useful because we will need to create an IAM IRSA (IAM role for service account) and attach it to the pod appropriately.
+
+##### Steps:
+
+1. [x] Create a Python application that uploads a file to an S3 bucket.
+3. [x] Dockerize the application.
+2. [x] Create a Kubernetes deployment for the Python application.
+3. [x] Define all the required IAM services (role, policy, service account-IRSA) for the Kubernetes deployment.
+4. [x] Initialize Git repository for version control.
+
+![codesnap](https://github.com/assafdori/pod-to-s3/blob/main/k9s.png)
+![codesnap](https://github.com/assafdori/pod-to-s3/blob/main/codesnap.png)
+
+
---
+++ infrastructure/openid-connect.tf
@@ -0,0 +1,80 @@
+resource "aws_iam_openid_connect_provider" "oidc" {
+	client_id_list = ["sts.amazonaws.com"]
+	thumbprint_list = [data.tls_certificate.cert.certificates.0.sha1_fingerprint]
+	url = data.tls_certificate.cert.url
+}
+
+
+data "tls_certificate" "cert" {
+	url = aws_eks_cluster.cluster.identity[0].oidc[0].issuer
+}
+
+
+resource "aws_iam_role" "oidc" {	
+	name = "${var.env}-oidc-role"
+	assume_role_policy = data.aws_iam_policy_document.oidc_assume_role_policy.json
+}
+
+
+data "aws_iam_policy_document" "oidc_assume_role_policy" {
+	statement {
+		actions = ["sts:AssumeRoleWithWebIdentity"]
+		effect = "Allow"
+
+
+		condition {
+			test = "StringEquals"
+			variable = "${replace(aws_iam_openid_connect_provider.oidc.url, "https://", "")}:sub"
+			values = ["system:serviceaccount:default:app"]
+		}
+		condition {
+			test = "StringEquals"
+			variable = "${replace(aws_iam_openid_connect_provider.oidc.url, "https://", "")}:aud"
+			values = ["sts.amazonaws.com"]
+		}
+
+		principals {
+			identifiers = [aws_iam_openid_connect_provider.oidc.arn]
+			type = "Federated"
+		}
+	}
+}
+
+
+
+resource "aws_iam_policy" "pod-to-s3-policy" {
+	name = "${var.env}-pod-to-s3-bucket"
+	policy = <<EOF
+{
+	"Version": "2012-10-17",
+	"Statement": [
+		{
+			"Effect": "Allow",
+			"Action": 
+			[
+				"s3:*"
+			],
+			"Resource": "*"
+		}
+	]
+}
+EOF
+}
+
+
+resource "aws_iam_role_policy_attachment" "oidc" {
+	role = aws_iam_role.oidc.name
+	policy_arn = aws_iam_policy.pod-to-s3-policy.arn
+
+
+}
+
+resource "aws_iam_role_policy_attachment" "s3-policy" {
+	role = aws_iam_role.oidc.name
+	policy_arn = aws_iam_policy.pod-to-s3-policy.arn
+
+}
+
+
+
+
